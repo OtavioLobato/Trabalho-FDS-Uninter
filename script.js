637d3bf632cf96f7417ecee587d65805dd078808
Otávio Procópio Lobato <97693912+OtavioLobato@users.noreply.github.com>
2025-05-17 10:32:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let botao = document.querySelector('#botao');
+let mensagem = document.querySelector('#mensagem')
+botao.addEventListener('click', ()=>{
+    mensagem.innerHTML = "Seja bem-vindo(a)! Espero que tenha um ótimo dia!" 
+});
